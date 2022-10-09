aa0918c68df7be55d3350c51b44e9bd5efe2e4fc
gowthamrazing <gowthamrazing@gmail.com>
2022-10-09 14:51:48 +0800

    Creating an Alert style alert Controller

--- UIAlert Messages/ViewController.swift
+++ UIAlert Messages/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func SubmitClicked(_ sender: Any) {
+        //Title - "Title of the alert box"
+        //Message - "Message to be shown in the alert box"
+        // UIAlertControllerStyle - "Style of the alert Controller"
+        let alert = UIAlertController(title: "Error", message: "Please Enter all Fields", preferredStyle: UIAlertController.Style.alert)
+        
+        //Button action to show the ok button in the alert
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) { (UIAlertAction) in
+            print("Ok button Clicked")
+        }
+        
+        // TO add the created button
+        alert.addAction(okButton)
+        
+        // Present the created alert viewcontroller
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
 
